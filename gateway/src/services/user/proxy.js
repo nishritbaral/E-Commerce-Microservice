ffd3a9d9b35ed3c0359dd3773cd8b343bb891a97
Nishrit Baral <nishritbaral@gmail.com>
2025-09-18 22:37:58 +0545

    added proxy to forward request for user

---
+++ gateway/src/services/user/proxy.js
@@ -0,0 +1,9 @@
+import { createProxyMiddleware } from "http-proxy-middleware";
+
+const userProxy = createProxyMiddleware({
+  target: "http://localhost:8001",
+  changeOrigin: true,
+  pathRewrite: { "^/user/": "" },
+});
+
+export default userProxy;
