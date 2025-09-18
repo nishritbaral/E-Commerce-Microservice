c246afd865377d029e01261139f8b7f131ae471e
Nishrit Baral <nishritbaral@gmail.com>
2025-09-18 15:54:25 +0545

    moves index.js

--- userService/src/index.js
+++ userService/src/index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
-import app from "./src/app.js";
-import connectDB from "./src/1_config/db.connect.js";
+import app from "./app.js";
+import connectDB from "./1_config/db.connect.js";
 
 dotenv.config();
 
