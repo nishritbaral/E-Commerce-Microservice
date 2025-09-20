94da7d467bf05ddc1078b1ff9ec5278ab8139485
Nishrit Baral <nishritbaral@gmail.com>
2025-09-20 21:58:04 +0545

    initialized product service

--- productService/src/index.js
+++ productService/src/index.js
@@ -1,9 +1,7 @@
-import express from "express";
 import dotenv from "dotenv";
+import app from "./app.js";
 
 dotenv.config();
-const app = express();
-app.use(express.json());
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
