8a386fe022f57e28c19151015158825a93020a87
Nishrit Baral <nishritbaral@gmail.com>
2025-09-30 21:50:59 +0545

    added constants

---
+++ productService/src/2_constants/product.constants.js
@@ -0,0 +1,14 @@
+const PRODUCT_CONSTANTS = {
+  CATEGORY: [
+    "clothing",
+    "grocery",
+    "kitchen",
+    "furniture",
+    "electronics",
+    "stationary",
+    "sports",
+    "others",
+  ],
+};
+
+export default PRODUCT_CONSTANTS;
