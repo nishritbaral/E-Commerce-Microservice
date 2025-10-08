19cefb3c9aa68df9061cb86f72ff839f09d7daa4
Nishrit Baral <nishritbaral@gmail.com>
2025-10-08 22:15:09 +0545

    added product routes

---
+++ productService/src/5_routes/product.routes.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const router = express.Router();
+
+router.post("/add", () => {});
+
+router.delete("/delete", () => {});
+
+router.put("/update", () => {});
+
+export { router as productRoutes };
--- productService/src/app.js
+++ productService/src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
+import { productRoutes } from "./5_routes/product.routes";
 
 const app = express();
 app.use(express.json());
+
+app.use(productRoutes);
 
 export default app;
