a58e6e5be8a7ab8a3553759f227ea664b6fab7a0
Nishrit Baral <nishritbaral@gmail.com>
2025-10-09 21:58:49 +0545

    updated add controller

--- productService/src/7_controller/product.controller.js
+++ productService/src/7_controller/product.controller.js
@@ -1,4 +1,15 @@
-export const addController = () => {};
+import { addService } from "../8_service/product.service";
+
+export const addController = async () => {
+  try {
+    const response = await addService();
+    return res.status(200).send({ message: response.message });
+  } catch (error) {
+    return res
+      .status(400)
+      .send({ message: "Failed to add the product.", error: error.message });
+  }
+};
 
 export const deleteController = () => {};
 
