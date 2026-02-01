1a6c90cb3f8900d1f8b41988d8aba04dd86bf05b
Nishrit Baral <nishritbaral@gmail.com>
2026-02-01 23:40:59 +0545

    added product schema

--- productService/src/3_models/product.schema.js
+++ productService/src/3_models/product.schema.js
@@ -1,6 +1,21 @@
 import mongoose from "mongoose";
+import PRODUCT_CONSTANTS from "../2_constants/product.constants.js";
 
-const productSchema = new mongoose.Schema({});
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true, trim: true, maxlength: 50 },
+  description: {
+    type: String,
+    required: true,
+    tirm: true,
+    maxlength: 300,
+  },
+  price: { type: Number, required: true },
+  category: { type: String, required: true, enum: PRODUCT_CONSTANTS },
+  image: {
+    type: String,
+    require: true,
+  },
+});
 
 const productTable = mongoose.model("product", productSchema);
 
