e5daf1aa19510e0421a726fae813c72a34e26da6
Nishrit Baral <nishritbaral@gmail.com>
2026-02-02 21:38:58 +0545

    edited some schema

--- productService/src/3_models/product.schema.js
+++ productService/src/3_models/product.schema.js
@@ -9,12 +9,15 @@ const productSchema = new mongoose.Schema({
     tirm: true,
     maxlength: 300,
   },
-  price: { type: Number, required: true },
   category: { type: String, required: true, enum: PRODUCT_CONSTANTS },
+  price: { type: Number, required: true, min: 0 },
+  quantity: { type: Number, required: true, max: 1 },
+  freeShipping: { type: Boolean, required: true },
   image: {
     type: String,
     require: true,
   },
+  sellerId: { type: mongoose.ObjectId, required: true },
 });
 
 const productTable = mongoose.model("product", productSchema);
